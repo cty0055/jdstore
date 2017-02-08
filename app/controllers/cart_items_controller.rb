a29d3fd9a845859344e3db114a651ae50c4af54b
cty <email@xxx.xxx>
2017-02-08 22:02:11 +0800

    change quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -7,6 +7,20 @@ class CartItemsController < ApplicationController
     @product = @cart_item.product
     @cart_item.destroy
 
+    def update
+        @cart = current_cart
+        @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+        @cart_item.update(cart_item_params)
+
+        redirect_to carts_path
+      end
+
+      private
+
+      def cart_item_params
+        params.require(:cart_item).permit(:quantity)
+      end
+
     flash[:warning] = "成功将 #{@product.title} 从购物车删除!"
     redirect_to :back
   end
